40f5692419668fc4568d3097c3f99f77bd7eff5a
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-03-02 13:41:27 -0500

    Adds title setter for application layout

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,23 +3,30 @@ class MainController < ApplicationController
   end
 
   def about
+    @page_title = "About"
   end
 
   def students
+    @page_title = "Students"
   end
 
   def faculty
+    @page_title = "Faculty"
   end
 
   def contact
+    @page_title = "Contact Us"
   end
 
   def terms
+    @page_title = "Terms of Use"
   end
 
   def site_map
+    @page_title = "Site Map"
   end
 
   def faq
+    @page_title = "Frequently Asked Questions"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def get_title(title)
+    title ||= ""
+    title = title.length > 0 ? title + " | College STAR" : "College STAR"
+  end
 end
