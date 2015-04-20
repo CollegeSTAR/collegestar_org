e276b6ab724ae6e5801905288695b4335d1b7304
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-04-20 15:25:56 -0400

    Adds file attachment uploads to campuses via paperclip

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -1,2 +1,4 @@
 class Campus < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "250x250>", :thumb => "75x75>" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
