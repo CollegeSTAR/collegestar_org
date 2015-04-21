6d385e6aea76e03dcabd70e779b55166251c8bf6
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-04-21 11:57:05 -0400

    Adds order_by_created_date_and_limit to news_articles. This is used to find, an optionally limited, stream of news articles.

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -1,6 +1,12 @@
 class NewsArticle < ActiveRecord::Base
   validates_presence_of :slug
-  
+
+  def self.order_by_created_date_and_limit(limit = 10, offset = 0)
+    articles = limit(limit)
+    articles = articles.offset(offset)
+    articles = articles.order(created_at: :desc)
+  end
+
   def to_param
     slug
   end
