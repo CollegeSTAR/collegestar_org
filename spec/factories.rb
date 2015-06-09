9329ff1e76415f208156ec7a4e6c8b37e40e3189
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-06-09 16:58:53 -0400

    Creates spec/factories.rb and spec/factories_spec to validate all factories.

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do 
+  factory :news_article do
+    sequence( :title ) { |n| "news article #{n}" }
+    sequence( :slug ) { |n| "news-article-#{n}" }
+    content "This is the content of this article."
+  end
+end
