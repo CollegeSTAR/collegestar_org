2be38c6cbb52b0cfdf642ae1f43d1b19939bdd95
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-06-12 14:04:22 -0400

    Adds migrations for user model that add password_digest to users table and add not null constraints to first_name, last_name and email of users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610185721) do
+ActiveRecord::Schema.define(version: 20150612175541) do
 
   create_table "campuses", force: :cascade do |t|
     t.string   "name",               null: false
@@ -67,11 +67,12 @@ ActiveRecord::Schema.define(version: 20150610185721) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
+    t.string   "email",           null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest", null: false
   end
 
 end
