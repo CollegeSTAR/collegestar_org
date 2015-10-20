c0f1295e116a730d59611c24574d24d6b6c67b4c
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-10-20 16:57:26 -0400

    Reverse order of state array
    It feels more natural to have the state name be the key and the
    abbreviation to be the value for the state array.

--- app/models/address/state.rb
+++ app/models/address/state.rb
@@ -1,85 +1,85 @@
 module Address::State
 
   @@states = {
-    "AL" => "Alabama",
-    "AK" => "Alaska",
-    "AZ" => "Arizona",         
-    "AR" => "Arkansas",        
-    "CA" => "California",      
-    "CO" => "Colorado",
-    "CT" => "Connecticut",    
-    "DE" => "Delaware",
-    "FL" => "Florida",      
-    "GA" => "Georgia",
-    "HI" => "Hawaii", 
-    "ID" => "Idaho",
-    "IL" => "Illinois",
-    "IN" => "Indiana",
-    "IA" => "Iowa",
-    "KS" => "Kansas",
-    "KY" => "Kentucky",
-    "LA" => "Louisiana",
-    "ME" => "Maine",
-    "MD" => "Maryland",
-    "MA" => "Massachusetts",
-    "MI" => "Michigan",
-    "MN" => "Minnesota",
-    "MS" => "Mississippi",
-    "MO" => "Missouri",
-    "MT" => "Montana",
-    "NE" => "Nebraska",
-    "NV" => "Nevada",
-    "NH" => "New Hampshire",
-    "NJ" => "New Jersey",
-    "NM" => "New Mexico",
-    "NY" => "New York",
-    "NC" => "North Carolina",
-    "ND" => "North Dakota",
-    "OH" => "Ohio",
-    "OK" => "Oklahoma",
-    "OR" => "Oregon",
-    "PA" => "Pennsylvania",
-    "RI" => "Rhode Island",
-    "SC" => "South Carolina",
-    "SD" => "South Dakota",
-    "TN" => "Tennessee",
-    "TX" => "Texas",
-    "UT" => "Utah",
-    "VT" => "Vermont",
-    "VA" => "Virginia", 
-    "WA" => "Washington",
-    "WV" => "West Virginia",
-    "WI" => "Wisconsin",
-    "WY" => "Wyoming"
+    "Alabama" => "AL",
+    "Alaska" => "AK",
+    "Arizona" => "AZ",
+    "Arkansas" => "AR",
+    "California" => "CA",
+    "Colorado" => "CO",
+    "Connecticut" => "CT",
+    "Delaware" => "DE",
+    "Florida" => "FL",
+    "Georgia" => "GA",
+    "Hawaii" => "HI",
+    "Idaho" => "ID",
+    "Illinois" => "IL",
+    "Indiana" => "IN",
+    "Iowa" => "IA",
+    "Kansas" => "KS",
+    "Kentucky" => "KY",
+    "Louisiana" => "LA",
+    "Maine" => "ME",
+    "Maryland" => "MD",
+    "Massachusetts" => "MA",
+    "Michigan" => "MI",
+    "Minnesota" => "MN",
+    "Mississippi" => "MS",
+    "Missouri" => "MO",
+    "Montana" => "MT",
+    "Nebraska" => "NE",
+    "Nevada" => "NV",
+    "New Hampshire" => "NH",
+    "New Jersey" => "NJ",
+    "New Mexico" => "NM",
+    "New York" => "NY",
+    "North Carolina" => "NC",
+    "North Dakota" => "ND",
+    "Ohio" => "OH",
+    "Oklahoma" => "OK",
+    "Oregon" => "OR",
+    "Pennsylvania" => "PA",
+    "Rhode Island" => "RI",
+    "South Carolina" => "SC",
+    "South Dakota" => "SD",
+    "Tennessee" => "TN",
+    "Texas" => "TX",
+    "Utah" => "UT",
+    "Vermont" => "VT",
+    "Virginia" => "VA",
+    "Washington" => "WA",
+    "West Virginia" => "WV",
+    "Wisconsin" => "WI",
+    "Wyoming" => "WY"
     }
 
-  def self.all
-    @@states 
+  def get_states(args = {})
+    return @@states
   end
 
-  def self.get_abbreviation(name)
-    @@states.each do |abbrev, state_name|
+  def get_state_abbreviation(name)
+    @@states.each do |state_name, abbrev|
       return abbrev if state_name.downcase == name.downcase
     end
     false
   end
 
-  def self.get_name(abbreviation)
-    @@states.each do |abbrev, state_name|
+  def get_state_name(abbreviation)
+    @@states.each do |state_name, abbrev|
       return state_name if abbrev.downcase == abbreviation.downcase
     end
     false
   end
 
-  def self.valid?(args = {})
+  def state_valid?(args = {})
     valid_name, valid_abbr, valid = false, false, false
     name = args.fetch(:name, false)
     abbr = args.fetch(:abbreviation, false)
     if name
-      valid_name = true if @@states.has_value? name
+      valid_name = true if @@states.has_key? name
     end
     if abbr
-      valid_abbr = true if @@states.has_key? abbr
+      valid_abbr = true if @@states.has_value? abbr
     end
     valid = true if valid_name && valid_abbr
     valid = true if valid_name && !abbr
