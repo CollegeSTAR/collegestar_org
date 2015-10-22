6ac92970a8a1cde519d6c622001b3a430ca4db27
Joel Tanner Jones <jonesjoe@ecu.edu>
2015-10-22 15:21:40 -0400

    Add Faculty inflector
    Rails was improperly inflecting the singular "faculty" to "faculties".
    Added an irregular clause to the config/initializers/inflections.rb

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -18,4 +18,5 @@
 ActiveSupport::Inflector.inflections do |inflect|
   inflect.singular /^(campus)(es)?$/i, '\1'
   inflect.plural    /^(campus)$/i, '\1es'
+  inflect.irregular 'faculty', 'faculty'
 end
