2f19e5db8542a385a57a08ed0846b383fe27f993
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-01-15 16:57:58 -0500

    Add UDL Module History controller
    Adds a module history controller and router spec.

---
+++ app/controllers/udl_module_history_controller.rb
@@ -0,0 +1,7 @@
+class UdlModuleHistoryController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- spec/routing/udl_module_history_routes_spec.rb
+++ spec/routing/udl_module_history_routes_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe UdlModuleHistoryController do
     end
 
     context 'named routes' do
-      it 'get: profile_module_index_path(1)' do
-        expect(get(profile_module_index_path(1))).to route_to('udl_module_history#index', profile_id: '1')
+      it 'get: profile_modules_path(1)' do
+        expect(get(profile_modules_path(1))).to route_to('udl_module_history#index', profile_id: '1')
       end
       it 'get: profile_module_path(1,1)' do
         expect(get(profile_module_path(1,1))).to route_to('udl_module_history#show', profile_id: '1', id: '1')
