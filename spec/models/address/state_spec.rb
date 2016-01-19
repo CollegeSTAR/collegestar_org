9e6bf43fc5169ee67fdf29ccf6bca638cbfd11d7
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-01-19 16:43:36 -0500

    Update state model spec
    Flips the key/value of state array and adds specs for valid? method.

--- spec/models/address/state_spec.rb
+++ spec/models/address/state_spec.rb
@@ -1,95 +1,119 @@
 require 'spec_helper'
 
-RSpec.describe State do
+RSpec.describe Address::State do
+  let(:address) { Class.new { extend Address::State  } }
+  let(:states_expected) { 
+    {
+      "Alabama" => "AL",
+      "Alaska" => "AK",
+      "Arizona" => "AZ",         
+      "Arkansas" => "AR",        
+      "California" => "CA",      
+      "Colorado" => "CO",
+      "Connecticut" => "CT",    
+      "Delaware" => "DE",
+      "Florida" => "FL",      
+      "Georgia" => "GA",
+      "Hawaii" => "HI", 
+      "Idaho" => "ID",
+      "Illinois" => "IL",
+      "Indiana" => "IN",
+      "Iowa" => "IA",
+      "Kansas" => "KS",
+      "Kentucky" => "KY",
+      "Louisiana" => "LA",
+      "Maine" => "ME",
+      "Maryland" => "MD",
+      "Massachusetts" => "MA",
+      "Michigan" => "MI",
+      "Minnesota" => "MN",
+      "Mississippi" => "MS",
+      "Missouri" => "MO",
+      "Montana" => "MT",
+      "Nebraska" => "NE",
+      "Nevada" => "NV",
+      "New Hampshire" => "NH",
+      "New Jersey" => "NJ",
+      "New Mexico" => "NM",
+      "New York" => "NY",
+      "North Carolina" => "NC",
+      "North Dakota" => "ND",
+      "Ohio" => "OH",
+      "Oklahoma" => "OK",
+      "Oregon" => "OR",
+      "Pennsylvania" => "PA",
+      "Rhode Island" => "RI",
+      "South Carolina" => "SC",
+      "South Dakota" => "SD",
+      "Tennessee" => "TN",
+      "Texas" => "TX",
+      "Utah" => "UT",
+      "Vermont" => "VT",
+      "Virginia" => "VA", 
+      "Washington" => "WA",
+      "West Virginia" => "WV",
+      "Wisconsin" => "WI",
+      "Wyoming" => "WY"
+   }
+ }
   describe "#all" do
     it "should return a hash with the abbreviation of state as the key and the state object as the value" do
-      @states = State.all
-      @states_expected = {
-        "AL" => "Alabama",
-        "AK" => "Alaska",
-        "AZ" => "Arizona",         
-        "AR" => "Arkansas",        
-        "CA" => "California",      
-        "CO" => "Colorado",
-        "CT" => "Connecticut",    
-        "DE" => "Delaware",
-        "FL" => "Florida",      
-        "GA" => "Georgia",
-        "HI" => "Hawaii", 
-        "ID" => "Idaho",
-        "IL" => "Illinois",
-        "IN" => "Indiana",
-        "IA" => "Iowa",
-        "KS" => "Kansas",
-        "KY" => "Kentucky",
-        "LA" => "Louisiana",
-        "ME" => "Maine",
-        "MD" => "Maryland",
-        "MA" => "Massachusetts",
-        "MI" => "Michigan",
-        "MN" => "Minnesota",
-        "MS" => "Mississippi",
-        "MO" => "Missouri",
-        "MT" => "Montana",
-        "NE" => "Nebraska",
-        "NV" => "Nevada",
-        "NH" => "New Hampshire",
-        "NJ" => "New Jersey",
-        "NM" => "New Mexico",
-        "NY" => "New York",
-        "NC" => "North Carolina",
-        "ND" => "North Dakota",
-        "OH" => "Ohio",
-        "OK" => "Oklahoma",
-        "OR" => "Oregon",
-        "PA" => "Pennsylvania",
-        "RI" => "Rhode Island",
-        "SC" => "South Carolina",
-        "SD" => "South Dakota",
-        "TN" => "Tennessee",
-        "TX" => "Texas",
-        "UT" => "Utah",
-        "VT" => "Vermont",
-        "VA" => "Virginia", 
-        "WA" => "Washington",
-        "WV" => "West Virginia",
-        "WI" => "Wisconsin",
-        "WY" => "Wyoming"
-      }
-      expect(@states).to eq(@states_expected)
+      expect(address.get_states).to match_array(states_expected)
     end
   end
-
   describe "#get_abbreviation" do
     it "should return an abbreviation for a given state name" do
-      north_carolina_abbreviation = State.get_abbreviation "North Carolina"
+      north_carolina_abbreviation = address.get_state_abbreviation "North Carolina"
       expect(north_carolina_abbreviation).to eq("NC")
     end
 
     it "should return an abbreviation for a given state name with lower case letters" do 
-      north_carolina_abbreviation = State.get_abbreviation "north carolina"
+      north_carolina_abbreviation = address.get_state_abbreviation "north carolina"
       expect(north_carolina_abbreviation).to eq("NC")
     end
 
     it "should return false for an unknown state name" do
-      fake_state_abbreviation = State.get_abbreviation "Fake State"
+      fake_state_abbreviation = address.get_state_abbreviation "Fake State"
       expect(fake_state_abbreviation).to be_falsey
     end
   end
+    describe "#valid?" do
+      it "should return true if state name is found" do
+        expect(address.state_valid? name: "North Carolina").to be_truthy
+      end
+      it "should return true if state abbreviation is found" do
+        expect(address.state_valid? abbreviation: "NC").to be_truthy
+      end
+      it "should return true if state abbreviation and name are found" do
+        expect(address.state_valid? abbreviation: "NC", name: "North Carolina").to be_truthy
+      end
+      it "should return false if state name is not found" do
+        expect(address.state_valid? name: "Not a State").to be_falsey
+      end
+      it "should return false if state abbreviation is not found" do
+        expect(address.state_valid? abbreviation: "Not an abbreviation").to be_falsey
+      end
+      it "should return false if state name is correct, but abbreviation is incorrect" do
+        expect(address.state_valid? abbreviation: "Not an abbreviation", name: "North Carolina").to be_falsey
+      end      
+      it "should return false if state name is incorrect, but abbreviation is correct" do
+        expect(address.state_valid? abbreviation: "NC", name: "Not a state").to be_falsey
+      end
+    end
 
   describe "#get_name" do
     it "should return a state name for a given state abbreviation" do
-      north_carolina_name = State.get_name "NC"
+      north_carolina_name = address.get_state_name "NC"
       expect(north_carolina_name).to eq("North Carolina")
     end
 
     it "should return a name for a given abbreviation with lower case letters" do 
-      north_carolina_name = State.get_name "nc"
+      north_carolina_name = address.get_state_name "nc"
       expect(north_carolina_name).to eq("North Carolina")
     end
 
     it "should return false for an unknown state abbreviation" do
-      fake_state_name = State.get_name "FS"
+      fake_state_name = address.get_state_name "FS"
       expect(fake_state_name).to be_falsey
     end
   end
