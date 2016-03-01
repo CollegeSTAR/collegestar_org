72dafc431772b77454649c17dd14f64fd9b96425
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-03-01 10:31:28 -0500

    Add basic information for Capistrano
    Adds info to Capfile and config/deploy*

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,14 @@
 lock '3.4.0'
 
 set :application, 'collegestar_org'
-set :repo_url, 'git@collegestar.org:me/collegestar_org.git'
+set :repo_url, 'git@collegestar.org:/home/git/collegestar_org.git'
+set :branch, 'master'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/deploy/collegestar_org'
 
 # Default value for :scm is :git
 # set :scm, :git
