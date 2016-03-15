4b68fbbd5454d148cf3e3fcfe3f269d8dd7aab38
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-03-15 13:49:01 -0400

    Style Campuses#index view
    Adds more styles for campuses index view as well as including the campus
    director's title in the contact field. Also updates the
    address_helper#formatted_address to look at address_2's length, not it's
    presence for including address_2 in the output.

--- app/helpers/address_helper.rb
+++ app/helpers/address_helper.rb
@@ -1,7 +1,7 @@
 module AddressHelper
   def format_address( model )
     address = "#{model.address_1} <br />"
-    address += "#{model.address_2} <br />" if model.address_2
+    address += "#{model.address_2} <br />" if model.address_2.length > 0
     address += "#{model.city}, #{model.state} #{model.zip}"
     address.html_safe
   end
