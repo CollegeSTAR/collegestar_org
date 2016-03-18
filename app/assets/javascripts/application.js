a522e4011316de1649f512b68a4e38f2e24b1c36
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-03-18 15:24:03 -0400

    Add jquery-ui to application.js/css
    We're only requiring the datepicker for application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require jquery-ui/datepicker
 //= require ckeditor/init
 //= require turbolinks
 //= require user_menu
