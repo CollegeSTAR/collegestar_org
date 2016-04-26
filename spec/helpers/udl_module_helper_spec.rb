997fa4f763fc171578918287beb88a8e2f3e4544
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-04-26 15:47:07 -0400

    Add Helper spec for UdlModuleHelper
    Adds a helper spec for the UdlModuleHelper with tests around the
    module_admin? method. This method takes a user as an argument and
    determines whether or not a user has module_admin privileges.

---
+++ spec/helpers/udl_module_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe UdlModuleHelper do
+  let(:admin) { create(:admin_user) }
+  let(:user) { create(:user) }
+  describe "#module_admin?" do
+    context "with admin user" do
+      it "should return true" do
+        expect( helper.module_admin?(admin) ).to be true
+      end
+    end
+    context "with normal user" do
+      it "should return false" do
+        expect( helper.module_admin?(user) ).to be false
+      end
+    end
+  end
+end
