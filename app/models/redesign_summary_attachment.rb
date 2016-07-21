483c23a68f87c78918c81405ad67edec925f5312
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-07-21 11:37:27 -0400

    Update redesign_summary_attachment
    Validation was broken and not allowing anything other than png files to
    upload. We corrected that.

--- app/models/redesign_summary_attachment.rb
+++ app/models/redesign_summary_attachment.rb
@@ -1,5 +1,16 @@
 class RedesignSummaryAttachment < ActiveRecord::Base
   has_attached_file :attachment
-  validates_attachment_content_type :attachment, content_type: /\Aimage/
+  validates_attachment :attachment, content_type: { content_type: [
+                                                                    "image/png",
+                                                                    "image/gif",
+                                                                    "image/jpeg",
+                                                                    "application/pdf",
+                                                                    "application/msword",
+                                                                    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                                                                    "application/vnd.ms-excel",
+                                                                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                                                                    "application/vnd.ms-powerpoint",
+                                                                    "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+                                                                  ]}
 
 end
