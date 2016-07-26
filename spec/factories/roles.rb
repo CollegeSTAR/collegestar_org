534d927bd64015ff368508d18b310f46d2bf869a
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-07-26 15:19:31 -0400

    Add admin users/roles
    In the switch to using pundit, we need factories for the sub admin roles
    (ex: news_article_admin) to test the pundit policies.

--- spec/factories/roles.rb
+++ spec/factories/roles.rb
@@ -12,8 +12,8 @@ FactoryGirl.define do
     subject "all"
   end
 
-  factory :event_admin_role, class: Role do
-    name "event_admin"
+  factory :events_admin_role, class: Role do
+    name "events_admin"
     action "manage"
     subject "event"
   end
@@ -24,9 +24,33 @@ FactoryGirl.define do
     subject "contact"
   end
 
-  factory :module_proposals_admin, class: Role do
+  factory :campus_admin_role, class: Role do
+    name "campus_admin"
+    action "manage"
+    subject "campus"
+  end
+  
+  factory :module_admin_role, class: Role do
+    name "modules_admin"
+    action "manage"
+    subject "udl_module"
+  end
+
+  factory :module_author_role, class: Role do
+    name "modules_author"
+    action "manage"
+    subject "udl_module"
+  end
+
+  factory :module_proposals_admin_role, class: Role do
     name "module_proposals_admin"
     action "manage"
     subject "module_proposal"
+  end  
+  
+  factory :news_articles_admin_role, class: Role do
+    name "news_articles_admin"
+    action "manage"
+    subject "news_article"
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -21,7 +21,30 @@ FactoryGirl.define do
     after(:create) { |user| user.roles << create(:admin_role) }
   end
 
-  factory :module_proposals_admin_user, class: User do
+  factory "campus_admin_user", class: User do
+    title "Mr."
+    first_name "Tanner_admin"
+    last_name "Jones_admin"
+    sequence(:email) { |n| "admin#{n}@collegestar.org" }
+    password "secret_pass"
+    password_confirmation "secret_pass"
+    phone_number "1236541234"
+    after(:create) { |user| user.roles << create(:campus_admin_role) }
+  end
+ 
+  factory "events_admin_user", class: User do
+    title "Mr."
+    first_name "Tanner_admin"
+    last_name "Jones_admin"
+    sequence(:email) { |n| "admin#{n}@example.org" }
+    password "secret_pass"
+    password_confirmation "secret_pass"
+    phone_number "1236541234"
+    after(:create) { |user| user.roles << create(:events_admin_role) }
+  end
+
+
+  factory :module_admin_user, class: User do
     title "Mr. "
     first_name "John"
     last_name "Smith"
@@ -29,6 +52,51 @@ FactoryGirl.define do
     password "secret_pass"
     password_confirmation "secret_pass"
     phone_number "1236541234"
-    after(:create) { |user| user.roles << create(:module_proposals_admin) }
+    after(:create) { |user| user.roles << create(:module_admin_role) }
+  end
+
+  factory :module_author_user, class: User do
+    title "Mr. "
+    first_name "John"
+    last_name "Smith"
+    email "john.j.smith@example.com"
+    password "secret_pass"
+    password_confirmation "secret_pass"
+    phone_number "1236541234"
+    after(:create) { |user| user.roles << create(:module_author_role) }
+  end
+
+  factory :module_author_user_2, class: User do
+    title "Ms. "
+    first_name "Jane"
+    last_name "Smith"
+    email "jane.smith@example.com"
+    password "secret_pass"
+    password_confirmation "secret_pass"
+    phone_number "1236541234"
+    after(:create) { |user| user.roles << create(:module_author_role) }
+  end
+
+  factory :module_proposals_admin_user, class: User do
+    title "Mr. "
+    first_name "John"
+    last_name "Smith"
+    email "john.m.smith@example.com"
+    password "secret_pass"
+    password_confirmation "secret_pass"
+    phone_number "1236541234"
+    after(:create) { |user| user.roles << create(:module_proposals_admin_role) }
+  end
+
+  factory :news_articles_admin_user, class: User do
+    title "Mr. "
+    first_name "John"
+    last_name "Smith"
+    email "john.article.smith@example.com"
+    password "secret_pass"
+    password_confirmation "secret_pass"
+    phone_number "1236541234"
+    after(:create) { |user| user.roles << create(:news_articles_admin_role) }
+
   end
 end
