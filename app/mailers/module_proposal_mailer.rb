2d266f79c5dd82789dbdfe8ed11eac406d08cdd8
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-11-03 10:54:00 -0400

    Add Ruben as a bcc to the module proposal confirmation

--- app/mailers/module_proposal_mailer.rb
+++ app/mailers/module_proposal_mailer.rb
@@ -3,8 +3,10 @@ class ModuleProposalMailer < ApplicationMailer
   def confirmation(proposal)
     @proposal = proposal
     @subject = "College STAR Module Proposal Received"
+    @admin_email = "WATSONR16@ECU.EDU"
     mail(
       to: @proposal.email,
+      bcc: @admin_email,
       subject: @subject
     )
   end
