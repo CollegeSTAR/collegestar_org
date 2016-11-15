8f7c8586b2e0018b0346074b87dd8a3736595d59
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-11-15 13:50:47 -0500

    Update sessions controller to redirect on login
    If a user is sent to the login view because they failed an authorize
    call, we want to remember where they were trying to go so we can try to
    send them there again after login. If they fail the authorization after
    being logged in, we handle that in the resource controller and redirect
    from there.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,9 @@ class SessionsController < ApplicationController
       permanent: params[:remember_me]
     )
     if @session.auth_token
-      redirect_to root_url
+      user_return_to = session[:user_return_to]
+      session.delete :user_return_to
+      redirect_to( user_return_to || root_url )
     else
       flash.now[:error] = "Incorrect username and/or password, please try again."
       render "new"
