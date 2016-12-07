7ca273589c60a1bed210e317db157c6ac87b7916
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-12-07 13:11:12 -0500

    Refactor UdlModuleSpec to use shoulda matchers
    Updates the validations and associations spec blocks to use shoulda
    matcher syntax.

--- spec/models/udl_module_spec.rb
+++ spec/models/udl_module_spec.rb
@@ -9,39 +9,19 @@ RSpec.describe UdlModule do
     let(:references_section) { create(:references_section) }
 
   describe "validations" do
-    it "validates presence of a title" do
-      is_expected.to validate_presence_of(:title)
-    end
-    it "validates uniqueness of title" do
-      is_expected.to validate_uniqueness_of(:title)
-    end
-    it "validates presence of slug" do
-      is_expected.to validate_presence_of(:slug)
-    end
-    it "validates uniqueness of slug" do
-      is_expected.to validate_uniqueness_of(:slug)
-    end
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title) }
+    it { should validate_presence_of(:slug) }
+    it { should validate_uniqueness_of(:slug) }
   end
 
   describe "associations" do
-    it "has many module_author_associations" do
-      is_expected.to have_many(:module_author_associations).with_foreign_key('module_id')
-    end    
-    it "has many modules_contributing_faculty" do
-      is_expected.to have_many(:module_faculty_associations).with_foreign_key('module_id')
-    end
-    it "has many module_section_associations" do
-      is_expected.to have_many(:module_section_associations).with_foreign_key('module_id')
-    end
-    it "has many authors" do
-      is_expected.to have_many(:authors).through(:module_author_associations).class_name('User')
-    end
-    it "has many faculty" do
-      is_expected.to have_many(:faculty).through(:module_faculty_associations).class_name('User')
-    end
-    it "has many sections" do
-      is_expected.to have_many(:sections).through(:module_section_associations).class_name('UdlModuleSection')
-    end
+    it { should have_many(:module_author_associations).with_foreign_key('module_id') }
+    it { should have_many(:module_faculty_associations).with_foreign_key('module_id') }
+    it { should have_many(:module_section_associations).with_foreign_key('module_id') }
+    it { should have_many(:authors).through(:module_author_associations).class_name('User') }
+    it { should have_many(:faculty).through(:module_faculty_associations).class_name('User') }
+    it { should have_many(:sections).through(:module_section_associations).class_name('UdlModuleSection') }
     it { should have_many(:assessment_questions)} 
   end
 
