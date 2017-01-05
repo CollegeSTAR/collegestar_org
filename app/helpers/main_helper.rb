b56f4ba3ae9c7bccca583cb356d27d15b93129d8
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-01-05 16:30:12 -0500

    Add bread crumb trail to public facing pages

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,2 +1,14 @@
 module MainHelper
+
+  def format_bread_crumb( args={} )
+    page_name = args.fetch(:page_name, "")
+    output = link_to( "home", root_path )
+    parents = args.fetch(:parents, [])
+   
+    parents.each do |parent|
+      output += ' &raquo; '.html_safe
+      output += link_to( "#{parent[0]}", parent[1] )
+    end
+    output += ' &raquo;'.html_safe + " #{page_name}"
+  end
 end
