3f436637f40132e6efd718a1865233f5ab4d1572
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-01-19 16:11:02 -0500

    Add more asset.precompile directives for ckeditor

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ module CollegestarOrg
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
+    
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
     config.assets.precompile += %w( ckeditor/**/* )
   end
 end
