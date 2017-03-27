3ea497b4849c703203fa39e8fb73618ba2949160
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-03-27 16:58:29 -0400

    Add faculty_nomination_surveys association to institutional_faculty
    Adds a has_many association.

--- spec/models/institutional_faculty_spec.rb
+++ spec/models/institutional_faculty_spec.rb
@@ -14,5 +14,6 @@ RSpec.describe InstitutionalFaculty do
     it { should belong_to(:campus) }
     it { should have_many(:faculty_course_associations).dependent(:destroy) }
     it { should have_many(:courses).through(:faculty_course_associations).source(:institutional_course) }
+    it { should have_many(:faculty_nomination_surveys) }
   end
 end
