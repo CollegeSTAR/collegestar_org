679cf1b25e97214dcfe18a505024b090d4e2ea1f
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-03-30 16:57:38 -0400

    Update department_faculty_associations for InstitutionalFaculty
    Adds a dependent: destroy option on the has_many association for
    department_faculty_associations.

--- app/models/institutional_faculty.rb
+++ app/models/institutional_faculty.rb
@@ -3,6 +3,7 @@ class InstitutionalFaculty < ActiveRecord::Base
   validates_uniqueness_of :email
 
   belongs_to :campus
+  has_many :department_faculty_associations, dependent: :destroy
   has_many :faculty_course_associations, dependent: :destroy
   has_many :courses, through: :faculty_course_associations, source: :institutional_course
   has_many :faculty_nomination_surveys
