0fb83ac4c9304d11584ebf981e89932f13e310ed
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-04-03 13:00:06 -0400

    Update faculty nomination surveys factory
    The FacultyNominationSurvey Model now requires:
    
      - faculty_first_name
      - faculty_last_name
      - department
    
    The department is an association so we also had to create a factory for
    the institutional_departments and wire up the association in the
    FacultyNominationSurvey factory.

--- spec/factories/faculty_nomination_surveys.rb
+++ spec/factories/faculty_nomination_surveys.rb
@@ -3,8 +3,12 @@ FactoryGirl.define do
     teaching_strategy "The strategy"
     strategy_description "The description"
     reasons_for_effectiveness "The reasons"
+    faculty_first_name "Jane"
+    faculty_last_name "Doe"
+
     
     #add institutional_faculty association
     institutional_faculty
+    association :department, factory: :institutional_department  
   end
 end
---
+++ spec/factories/institutional_department.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+
+  factory :institutional_department do
+    name "Test Department"
+    slug "test-department"
+  end
+end
