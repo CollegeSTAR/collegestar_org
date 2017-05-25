02063e214af2f3369dad3118abb9b475225d5a40
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-05-25 16:16:24 -0400

    Update strong parameters for UserModuleAssessment
    Rails 5.1 has a new feature for supporting arbitrary hashes that we'll
    be using to permit the selected_answer_choices for an assessment.
    
    https://github.com/rails/rails/commit/e86524c0c5a26ceec92895c830d1355ae47a7034

--- app/controllers/user_module_assessments_controller.rb
+++ app/controllers/user_module_assessments_controller.rb
@@ -1,8 +1,12 @@
 class UserModuleAssessmentsController < ApplicationController
 
+  def create
+    @submitted_params = user_module_assessment_params
+  end
+
   private
 
   def user_module_assessment_params
-    
+    params.require(:user_module_assessment).permit(selected_answer_choices: {}) 
   end
 end
