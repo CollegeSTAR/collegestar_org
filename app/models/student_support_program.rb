cb6535563a7e3dc16662ae0af279d30e5a4355b6
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-07-17 13:17:16 -0400

    Add to_param for student_support_programs
    StudentSupportProgram#to_param returns slug

--- app/models/student_support_program.rb
+++ app/models/student_support_program.rb
@@ -1,4 +1,8 @@
 class StudentSupportProgram < ActiveRecord::Base
   validates :name, :slug, presence: true
   validates :name, :slug, uniqueness: true
+
+  def to_param
+    self.slug
+  end
 end
--- spec/models/student_support_program_spec.rb
+++ spec/models/student_support_program_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe StudentSupportProgram do
-  
+  let(:program) { create(:student_support_program) }
+
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:slug) }
     it { should validate_uniqueness_of(:name) }
     it { should validate_uniqueness_of(:slug) }
+  end
+
+  describe "to_param" do
+    it "should return slug" do
+      expect(program.to_param).to eq(program.slug)  
+    end
   end
 end
