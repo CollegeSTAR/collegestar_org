f407edd3669579c99b7693d3afbd6a71b8ef3cd6
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-07-19 16:21:07 -0400

    Add rspec support for feature specs with file uploads

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -53,4 +53,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Store paperclip attachments here so that we can delete them after every test
+  Paperclip::Attachment.default_options[:path] = "#{Rails.root}/spec/test_files/:class/:id_partition/:style.:extension"
 end
