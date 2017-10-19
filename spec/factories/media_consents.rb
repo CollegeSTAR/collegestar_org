4849e46af424f9f7d3b87a8377327791731a9488
Joel Tanner Jones <jonesjoe@ecu.edu>
2017-10-19 15:19:18 -0400

    Add paperclip params for image attribute of media consents factory

--- spec/factories/media_consents.rb
+++ spec/factories/media_consents.rb
@@ -2,6 +2,9 @@ FactoryGirl.define do
   factory :media_consent do
     first_name "John"
     last_name "Smith"
+    image_file_name 'upload.png'
+    image_content_type 'image/png'
+    image_file_size '1024'
     consent true
   end
 end
