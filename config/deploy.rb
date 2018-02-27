7adadb7a37749bf92fa0236365a0e464fd17160a
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-02-27 14:54:22 -0500

    Add puma restart command to cap deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,10 +43,7 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      execute "bundle exec pumactl -F config/puma/production.rb phased-restart"
     end
   end
 
