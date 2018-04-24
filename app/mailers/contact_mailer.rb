fca79e7e5189eaf407a162adeafb384402f1b385
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-04-24 14:55:32 -0400

    Attempt 3 at X-SMTPAPI headers

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,14 +3,7 @@ class ContactMailer < ApplicationMailer
   def contact_us(contact_id)
     @page_content = Page.guaranteed_find slug: 'contacts-mailer-contact-us'
     @contact = Contact.find contact_id
-    headers({
-      'X-SMTPAPI' => JSON.generate({
-        unique_args: {
-          email_template: 'contact_us'
-        }
-      }).to_s
-    })
-
+    headers['X-SMTPAPI'] = '{"unique_args": {"category": "contact_us" }}'
     mail(
       to: @contact.email,
       subject: @contact.subject
