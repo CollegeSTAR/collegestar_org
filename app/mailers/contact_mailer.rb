83bbea455e7e4ee13e27cdd432ba23f1e1641cc4
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-04-24 16:03:10 -0400

    Add working SMTPAPI headers
    We send this email through sidekiq and in the process of trouble
    shooting this email, I discovered that we need to be rebooting sidekiq
    on deploys and we haven't been. That is why my changes were not working,
    sidekiq still had an old version of the app running in memory.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,10 @@ class ContactMailer < ApplicationMailer
   def contact_us(contact_id)
     @page_content = Page.guaranteed_find slug: 'contacts-mailer-contact-us'
     @contact = Contact.find contact_id
-    headers['X-SMTPAPI'] = '{"category": "ContactUs"}' 
+    headers['X-SMTPAPI'] = {
+      category: "ContactUs",
+      unique_args: { contact_id: contact_id }
+    }.to_json
     
     mail(
       to: @contact.email,
