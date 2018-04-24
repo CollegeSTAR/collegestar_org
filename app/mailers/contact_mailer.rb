2798ba9dcc1ce80ae34a582084dae6108f974887
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-04-24 15:05:06 -0400

    And fourth times'a charm.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,8 @@ class ContactMailer < ApplicationMailer
   def contact_us(contact_id)
     @page_content = Page.guaranteed_find slug: 'contacts-mailer-contact-us'
     @contact = Contact.find contact_id
-    headers['X-SMTPAPI'] = '{"unique_args": {"category": "contact_us" }}'
+    headers['X-SMTPAPI'] = '{"category": "ContactUs"}' 
+    
     mail(
       to: @contact.email,
       subject: @contact.subject
