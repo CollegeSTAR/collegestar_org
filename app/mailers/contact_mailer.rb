516d2dfaf594903f26a7abdc13544d39aa21fc16
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-04-24 14:21:31 -0400

    Add unique_args to contact_us mailer
    Sendgrid should send these along with the event webhook. We'll use these
    to reconnect and email with the resource that initiated it.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,10 @@ class ContactMailer < ApplicationMailer
   def contact_us(contact_id)
     @page_content = Page.guaranteed_find slug: 'contacts-mailer-contact-us'
     @contact = Contact.find contact_id
-
+    headers( unique_args: {
+        mailer: "ContactMailer"
+      }
+    )
     mail(
       to: @contact.email,
       subject: @contact.subject
