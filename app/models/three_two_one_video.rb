7cb1dd0348e13fb47e6e3a8d27fbf45d986fe170
Joel Tanner Jones <jonesjoe@ecu.edu>
2018-07-16 10:35:58 -0400

    Add styles to Three Two One videos.

--- app/models/three_two_one_video.rb
+++ app/models/three_two_one_video.rb
@@ -2,8 +2,8 @@ class ThreeTwoOneVideo < ActiveRecord::Base
   validates :title, :slug, presence: true
   has_one_attached :screenshot
 
-  scope :released, -> { where( released: true ) }
-  scope :unreleased, -> { where( released: false ) }
+  scope :released, -> { where( released: true ).order(:created_at) }
+  scope :unreleased, -> { where( released: false ).order(:created_at) }
 
   def to_param
     self.slug
