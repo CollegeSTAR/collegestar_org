01c2832005d2147126c4eea39f70940c92d0d314
Joel Tanner Jones <jonesjoe@ecu.edu>
2019-03-05 16:05:15 -0500

    Add delegation from UdlModuleFeedback to UdlModule
    Delegates the UdlModule#title to UdlModuleFeedback#udl_module_title

--- app/models/udl_module_feedback.rb
+++ app/models/udl_module_feedback.rb
@@ -3,4 +3,6 @@ class UdlModuleFeedback < ActiveRecord::Base
   validates :udl_module, presence: true
   belongs_to :udl_module
   belongs_to :user
+
+  delegate :title, to: :udl_module, prefix: true
 end
--- spec/models/udl_module_feedback_spec.rb
+++ spec/models/udl_module_feedback_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe UdlModuleFeedback do
     it { should belong_to(:udl_module) }
     it { should belong_to(:user) }
   end
+
+  describe "delegation" do
+    it { should delegate_method(:title).to(:udl_module).with_prefix }
+  end
 end
